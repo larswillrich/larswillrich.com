d33e456548d0ff11f32dc370a89c87923b0498a8
Lars Willrich <lars.willrich@share-now.com>
2020-04-16 12:49:58 +0200

    using nameserver as given in the zone

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -19,3 +19,33 @@ resource "aws_s3_bucket" "lars-willrich-static-website" {
 EOF
   }
 }
+
+resource "aws_route53_zone" "main" {
+  name = var.domain
+}
+
+resource "aws_route53_record" "root_domain" {
+  zone_id = aws_route53_zone.main.zone_id
+  name = var.domain
+  type = "A"
+
+  alias {
+    name    = aws_s3_bucket.lars-willrich-static-website.website_domain
+    zone_id = aws_s3_bucket.lars-willrich-static-website.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "website-ns" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = "larswillrich.com"
+  type    = "NS"
+  ttl     = "172800"
+
+  records = [
+    "${aws_route53_zone.main.name_servers.0}",
+    "${aws_route53_zone.main.name_servers.1}",
+    "${aws_route53_zone.main.name_servers.2}",
+    "${aws_route53_zone.main.name_servers.3}",
+  ]
+}
