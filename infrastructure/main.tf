0d7ec33179c29fa3889096a865697283f90f1187
Lars Willrich <lars.willrich@share-now.com>
2020-04-20 16:23:20 +0200

    extracted logic to module

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,51 +1,19 @@
-resource "aws_s3_bucket" "lars-willrich-static-website" {
-  bucket = "larswillrich.com"
-  acl    = "public-read"
-  policy = file("policy.json")
 
-  website {
-    index_document = "index.html"
-    error_document = "error.html"
 
-    routing_rules = <<EOF
-[{
-    "Condition": {
-        "KeyPrefixEquals": "docs/"
-    },
-    "Redirect": {
-        "ReplaceKeyPrefixWith": "documents/"
-    }
-}]
-EOF
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "terraform-state-lars-willrich"
+    key            = "larswillrich/personalWebsite/terraform.tfstate"
+    region         = "eu-central-1"
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "terraform-state-locks"
+    encrypt        = true
   }
 }
 
-resource "aws_route53_zone" "main" {
-  name = var.domain
-}
-
-resource "aws_route53_record" "root_domain" {
-  zone_id = aws_route53_zone.main.zone_id
-  name = var.domain
-  type = "A"
-
-  alias {
-    name    = aws_s3_bucket.lars-willrich-static-website.website_domain
-    zone_id = aws_s3_bucket.lars-willrich-static-website.hosted_zone_id
-    evaluate_target_health = false
-  }
-}
-
-resource "aws_route53_record" "website-ns" {
-  zone_id = aws_route53_zone.main.zone_id
-  name    = "larswillrich.com"
-  type    = "NS"
-  ttl     = "172800"
-
-  records = [
-    "${aws_route53_zone.main.name_servers.0}",
-    "${aws_route53_zone.main.name_servers.1}",
-    "${aws_route53_zone.main.name_servers.2}",
-    "${aws_route53_zone.main.name_servers.3}",
-  ]
+module "static-website" {
+  source              = "github.com/larswillrich/tf-aws-static-website-module"
+  domain              = "larswillrich.com"
+  default_root_object = "index.html"
 }
